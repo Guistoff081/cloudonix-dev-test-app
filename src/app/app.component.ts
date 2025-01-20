ff92c76adc9f30474984693211afd4fff2fea107
Guistoff081 <55218750+Guistoff081@users.noreply.github.com>
2025-01-19 21:46:00 -0300

    feat: Implement authentication-based layout rendering
    - Updated app.component.html to conditionally display layout or router outlet based on authentication status.
    - Enhanced app.component.ts to include AuthService for managing authentication state.
    - Introduced Observable for tracking user authentication and logging status changes.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,22 @@
+import { CommonModule } from '@angular/common';
+import { Observable } from 'rxjs';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
+import { AuthService } from '@core/auth.service';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, LayoutComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'cloudonix-dev-test-app';
+  isAuthenticated$: Observable<boolean>;
+  constructor(private authService: AuthService) {
+    this.isAuthenticated$ = this.authService.isLoggedIn();
+    this.isAuthenticated$.subscribe((isAuthenticated) => {
+      console.log('isAuthenticated', isAuthenticated);
+    });
+  }
 }
